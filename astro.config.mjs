bac308e59efeb44dc8c2963fe5947ba9795b1972
Pachi-Dev <pachi.claros@gmail.com>
2025-04-01 15:50:52 -0600

    Agregar integraciones de sitemap y robots.txt en la configuración de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,13 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import astroI18next from "astro-i18next";
 
+import sitemap from '@astrojs/sitemap';
+
+import aiRobotsTxt from 'astro-ai-robots-txt';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(),astroI18next()], 
+  integrations: [react(), astroI18next(), sitemap(), aiRobotsTxt()], 
 
   vite: {
     plugins: [tailwindcss()]
