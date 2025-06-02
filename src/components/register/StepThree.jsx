7cb40d3e31b4039a72929283899759dca4f87013
Pachi-Dev <pachi.claros@gmail.com>
2025-06-01 22:15:06 -0600

    Actualizar la URL del servidor en los componentes de registro a amofexpo.igeco.mx

--- src/components/register/StepThree.jsx
+++ src/components/register/StepThree.jsx
@@ -66,7 +66,7 @@ export function StepThree({ translates }) {
 
   const urlbase = import.meta.env.DEV
     ? 'http://localhost:3010/'
-    : 'https://industrialtransformation.mx/server/'
+    : 'https://amofexpo.igeco.mx/server/'
 
   async function saveUser() {
     setProcessing(true)
