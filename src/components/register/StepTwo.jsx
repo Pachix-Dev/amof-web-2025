03ddf487859ef587aebd323dba0b87218ffe52be
Pachi-Dev <pachi.claros@gmail.com>
2025-06-01 21:14:18 -0600

    Agregar nuevas áreas y posiciones en las traducciones para el formulario de registro

--- src/components/register/StepTwo.jsx
+++ src/components/register/StepTwo.jsx
@@ -159,9 +159,6 @@ export function StepTwo({ translates }) {
           >
             <option value=''>{translates.select_option}</option>
             <option value='INGENIERIA'>{translates.industry_option_1}</option>
-            <option value='SECTOR EDUCATIVO'>
-              {translates.industry_option_2}
-            </option>
             <option value='AUTOMOTRIZ'>{translates.industry_option_3}</option>
             <option value='ELECTRICIDAD Y ELECTRÓNICA'>
               {translates.industry_option_4}
@@ -204,6 +201,9 @@ export function StepTwo({ translates }) {
             <option value='BIOTECNOLOGÍA Y FARMACÉUTICA'>
               {translates.industry_option_18}
             </option>
+            <option value='SECTOR EDUCATIVO'>
+              {translates.industry_option_2}
+            </option>
           </select>
           {errors.industry && (
             <p className='text-[#ffe200] font-light'>
@@ -226,18 +226,34 @@ export function StepTwo({ translates }) {
             className='mt-2 w-full rounded-lg bg-transparent border border-gray-200 p-4 pe-12 text-sm text-white *:text-black'
           >
             <option value=''>{translates.select_option}</option>
-            <option value={translates.position_1}>
-              {translates.position_1}
-            </option>
-            <option value={translates.position_2}>
-              {translates.position_2}
-            </option>
-            <option value={translates.position_3}>
-              {translates.position_3}
-            </option>
-            <option value={translates.position_4}>
-              {translates.position_4}
-            </option>
+            {industry === 'SECTOR EDUCATIVO' ? (
+              <>
+                <option value={translates.position_5}>
+                  {translates.position_5}
+                </option>
+                <option value={translates.position_6}>
+                  {translates.position_6}
+                </option>
+                <option value={translates.position_7}>
+                  {translates.position_7}
+                </option>
+              </>
+            ) : (
+              <>
+                <option value={translates.position_1}>
+                  {translates.position_1}
+                </option>
+                <option value={translates.position_2}>
+                  {translates.position_2}
+                </option>
+                <option value={translates.position_3}>
+                  {translates.position_3}
+                </option>
+                <option value={translates.position_4}>
+                  {translates.position_4}
+                </option>
+              </>
+            )}
           </select>
           {errors.position && (
             <p className='text-[#ffe200] font-light'>
@@ -258,11 +274,22 @@ export function StepTwo({ translates }) {
             className='mt-2 w-full rounded-lg bg-transparent border border-gray-200 p-4 pe-12 text-sm text-white *:text-black uppercase'
           >
             <option value=''>{translates.select_option}</option>
-            {translates.list_area.map((area, index) => (
-              <option key={index} value={area}>
-                {area}
-              </option>
-            ))}
+            {industry === 'SECTOR EDUCATIVO' ? (
+              <>
+                <option value={translates.area_1}>{translates.area_1}</option>
+                <option value={translates.area_2}>{translates.area_2}</option>
+                <option value={translates.area_3}>{translates.area_3}</option>
+                <option value={translates.area_4}>{translates.area_4}</option>
+              </>
+            ) : (
+              <>
+                {translates.list_area.map((area, index) => (
+                  <option key={index} value={area}>
+                    {area}
+                  </option>
+                ))}
+              </>
+            )}
           </select>
           {errors.area && (
             <p className='text-[#ffe200] font-light'>{errors.area.message}</p>
