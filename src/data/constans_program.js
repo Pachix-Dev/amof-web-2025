64ed95c1627a984d5de1e9e6ee685ad487bbdf63
Catasi <aideecasillas14@gmail.com>
2025-08-05 12:00:58 -0600

    actualizacion programa amof

--- src/data/constans_program.js
+++ src/data/constans_program.js
@@ -5,12 +5,12 @@ export const programs = [
     date_en: "November 11th",
     events: [
       {
-        title: "",
-        title_en: "",
+        title: "Inauguración ITM 2025",
+        title_en: "ITM Opening 2025",
         description: "",
         description_en: "",
-        time: "15:00 hrs",
-        duration: "35 min",
+        time: "11:00 hrs",
+        duration: "60 min",
         speakers: [],
       },
       {
@@ -69,53 +69,42 @@ export const programs = [
           "Panorama Internacional de la Movilidad Eléctrica: Caminos hacia la Descarbonización",
         title_en:
           "International Panorama of Electric Mobility: Paths to Carbon Neutrality",
-        description: "",
-        description_en: "",
+        description:
+          "El panel reunirá a especialistas internacionales para reflexionar sobre enfoques, retos y avances en la implementación de la movilidad eléctrica. La conversación abordará temas estratégicos como políticas públicas, esquemas de colaboración público-privada, incentivos, infraestructura de carga y experiencias prácticas.",
+        description_en:
+          "The panel will gather international experts to reflect on approaches, challenges, and advances in the implementation of electric mobility. The conversation will address strategic topics such as public policy, public-private partnership schemes, incentives, load infrastructure, and practical experiences.",
         time: "12:15 hrs",
         duration: "60 min",
         speakers: [
-          // {
-          //   id: "1",
-          //   name: "Carlos Hernandez",
-          //   position: "Director de Relaciones Públicas",
-          //   position_en: "Director of Public Relations",
-          //   company: "IGECO",
-          //   bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat varius ex vel fermentum. Cras eu ex odio. Maecenas imperdiet dignissim magna quis sollicitudin. Etiam porttitor ligula ac neque egestas, nec blandit massa ullamcorper. Nam faucibus, felis at cursus gravida, leo tellus mollis sapien, at fringilla neque nibh a metus. Curabitur rhoncus orci ac ante efficitur facilisis. Etiam porttitor eros a malesuada pellentesque. Aenean nec semper ipsum. Donec pulvinar dapibus urna. Maecenas dignissim sollicitudin metus, quis convallis justo bibendum id. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat varius ex vel fermentum. Cras eu ex odio. Maecenas imperdiet dignissim magna quis sollicitudin. Etiam porttitor ligula ac neque egestas, nec blandit massa ullamcorper. Nam faucibus, felis at cursus gravida, leo tellus mollis sapien, at fringilla neque nibh a metus. Curabitur rhoncus orci ac ante efficitur facilisis. Etiam porttitor eros a malesuada pellentesque. Aenean nec semper ipsum. Donec pulvinar dapibus urna. Maecenas dignissim sollicitudin metus, quis convallis justo bibendum id.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat varius ex vel fermentum. Cras eu ex odio. Maecenas imperdiet dignissim magna quis sollicitudin. Etiam porttitor ligula ac neque egestas, nec blandit massa ullamcorper. Nam faucibus, felis at cursus gravida, leo tellus mollis sapien, at fringilla neque nibh a metus. Curabitur rhoncus orci ac ante efficitur facilisis. Etiam porttitor eros a malesuada pellentesque. Aenean nec semper ipsum. Donec pulvinar dapibus urna. Maecenas dignissim sollicitudin metus, quis convallis justo bibendum id.",
-          //   bio_en:
-          //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat varius ex vel fermentum. Cras eu ex odio. Maecenas imperdiet dignissim magna quis sollicitudin. Etiam porttitor ligula ac neque egestas, nec blandit massa ullamcorper. Nam faucibus, felis at cursus gravida, leo tellus mollis sapien, at fringilla neque nibh a metus. Curabitur rhoncus orci ac ante efficitur facilisis. Etiam porttitor eros a malesuada pellentesque. Aenean nec semper ipsum. Donec pulvinar dapibus urna. Maecenas dignissim sollicitudin metus, quis convallis justo bibendum id.",
-          //   image: "/img/speakers/alejandro_malagon.webp",
-          //   email: "ejemplo@gmail.com",
-          //   phone: "123456789",
-          //   moderador: true,
-          // },
-          // {
-          //   id: "2",
-          //   name: "Valentina González",
-          //   position: "Directora de Asociaciones",
-          //   position_en: "Association Director",
-          //   company: "Telmex",
-          //   bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat varius ex vel fermentum. Cras eu ex odio. Maecenas imperdiet dignissim magna quis sollicitudin. Etiam porttitor ligula ac neque egestas, nec blandit massa ullamcorper. Nam faucibus, felis at cursus gravida, leo tellus mollis sapien, at fringilla neque nibh a metus. Curabitur rhoncus orci ac ante efficitur facilisis. Etiam porttitor eros a malesuada pellentesque. Aenean nec semper ipsum. Donec pulvinar dapibus urna. Maecenas dignissim sollicitudin metus, quis convallis justo bibendum id. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat varius ex vel fermentum. Cras eu ex odio. Maecenas imperdiet dignissim magna quis sollicitudin. Etiam porttitor ligula ac neque egestas, nec blandit massa ullamcorper. Nam faucibus, felis at cursus gravida, leo tellus mollis sapien, at fringilla neque nibh a metus. Curabitur rhoncus orci ac ante efficitur facilisis. Etiam porttitor eros a malesuada pellentesque. Aenean nec semper ipsum. Donec pulvinar dapibus urna. Maecenas dignissim sollicitudin metus, quis convallis justo bibendum id.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat varius ex vel fermentum. Cras eu ex odio. Maecenas imperdiet dignissim magna quis sollicitudin. Etiam porttitor ligula ac neque egestas, nec blandit massa ullamcorper. Nam faucibus, felis at cursus gravida, leo tellus mollis sapien, at fringilla neque nibh a metus. Curabitur rhoncus orci ac ante efficitur facilisis. Etiam porttitor eros a malesuada pellentesque. Aenean nec semper ipsum. Donec pulvinar dapibus urna. Maecenas dignissim sollicitudin metus, quis convallis justo bibendum id.",
-          //   bio_en:
-          //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat varius ex vel fermentum. Cras eu ex odio. Maecenas imperdiet dignissim magna quis sollicitudin. Etiam porttitor ligula ac neque egestas, nec blandit massa ullamcorper. Nam faucibus, felis at cursus gravida, leo tellus mollis sapien, at fringilla neque nibh a metus. Curabitur rhoncus orci ac ante efficitur facilisis. Etiam porttitor eros a malesuada pellentesque. Aenean nec semper ipsum. Donec pulvinar dapibus urna. Maecenas dignissim sollicitudin metus, quis convallis justo bibendum id.",
-          //   image: "/img/speakers/Aida_Festo.webp",
-          //   email: "ejemplo@gmail.com",
-          //   phone: "123456789",
-          //   moderador: true,
-          // },
-          // {
-          //   id: "3",
-          //   name: "Alexander Murphy",
-          //   position: "CEO",
-          //   position_en: "CEO",
-          //   company: "AeroVironment",
-          //   bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat varius ex vel fermentum. Cras eu ex odio. Maecenas imperdiet dignissim magna quis sollicitudin. Etiam porttitor ligula ac neque egestas, nec blandit massa ullamcorper. Nam faucibus, felis at cursus gravida, leo tellus mollis sapien, at fringilla neque nibh a metus. Curabitur rhoncus orci ac ante efficitur facilisis. Etiam porttitor eros a malesuada pellentesque. Aenean nec semper ipsum. Donec pulvinar dapibus urna. Maecenas dignissim sollicitudin metus, quis convallis justo bibendum id. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat varius ex vel fermentum. Cras eu ex odio. Maecenas imperdiet dignissim magna quis sollicitudin. Etiam porttitor ligula ac neque egestas, nec blandit massa ullamcorper. Nam faucibus, felis at cursus gravida, leo tellus mollis sapien, at fringilla neque nibh a metus. Curabitur rhoncus orci ac ante efficitur facilisis. Etiam porttitor eros a malesuada pellentesque. Aenean nec semper ipsum. Donec pulvinar dapibus urna. Maecenas dignissim sollicitudin metus, quis convallis justo bibendum id.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat varius ex vel fermentum. Cras eu ex odio. Maecenas imperdiet dignissim magna quis sollicitudin. Etiam porttitor ligula ac neque egestas, nec blandit massa ullamcorper. Nam faucibus, felis at cursus gravida, leo tellus mollis sapien, at fringilla neque nibh a metus. Curabitur rhoncus orci ac ante efficitur facilisis. Etiam porttitor eros a malesuada pellentesque. Aenean nec semper ipsum. Donec pulvinar dapibus urna. Maecenas dignissim sollicitudin metus, quis convallis justo bibendum id.",
-          //   bio_en:
-          //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat varius ex vel fermentum. Cras eu ex odio. Maecenas imperdiet dignissim magna quis sollicitudin. Etiam porttitor ligula ac neque egestas, nec blandit massa ullamcorper. Nam faucibus, felis at cursus gravida, leo tellus mollis sapien, at fringilla neque nibh a metus. Curabitur rhoncus orci ac ante efficitur facilisis. Etiam porttitor eros a malesuada pellentesque. Aenean nec semper ipsum. Donec pulvinar dapibus urna. Maecenas dignissim sollicitudin metus, quis convallis justo bibendum id.",
-          //   image: "/img/speakers/arturo_oropeza.webp",
-          //   email: "ejemplo@gmail.com",
-          //   phone: "123456789",
-          //   moderador: true,
-          // },
+          {
+            id: "1",
+            name: "Hugo Isaak",
+            position: "Miembro del Grupo Global de Expertos en Smart Cities",
+            position_en: "Member of the Global Experts Group on Smart Cities",
+            company: "ONU-Hábitat",
+            bio: "El Dr. Hugo Isaak es un líder reconocido en ciudades inteligentes y desarrollo urbano sostenible. Su formación académica incluye un doctorado en Administración Pública por la Universidad Anáhuac y la Universidad de París-Sorbona, con especialización en Urbanismo, Medio Ambiente, Economía, Desarrollo Sostenible e Inteligencia Artificial.<br><br>Experiencia Profesional: El Dr. Isaak ha desempeñado roles clave en el sector público y privado, incluyendo su nombramiento en el Grupo Global de Expertos de Smart Cities de la ONU-Hábitat, donde lidera las Guías para el Desarrollo de Ciudades Inteligentes centradas en las personas. Es fundador y CEO de 'Neurabitat', un proyecto que utiliza inteligencia artificial y análisis de datos para mejorar la interacción entre las ciudades y sus habitantes.<br><br>En la Secretaría de Relaciones Exteriores (SRE), fue Coordinador Internacional General Urbano y autor del proyecto ICI (Interconectando Ciudades Inteligentes), contribuyendo a políticas públicas sostenibles. Destacan sus negociaciones para extender el mandato de México como Presidente de la Asamblea de la ONU-Hábitat hasta 2025 y la asignación y próxima celebración del Día Mundial del Hábitat en Querétaro.<br><br>Visión y Futuro: El Dr. Isaak está comprometido con la creación de ciudades más inteligentes y sostenibles. Su visión incluye Centros de Innovación de la Ciudad Inteligente y Agencias de Transformación Digital en México y LATAM, integrando tecnologías emergentes para beneficiar a todos los ciudadanos y promover la sostenibilidad. <br><br>En resumen, el Dr. Hugo Isaak es una figura influyente con una trayectoria ejemplar y una visión clara hacia un futuro más sostenible e inclusivo.",
+            bio_en:
+              "Dr. Hugo Isaak is a recognized leader in smart cities and sustainable urban development. His academic background includes a PhD in Public Administration from the University of Anáhuac and the University of Paris-Sorbonne, with specialization in Urbanism, Environment, Economy, Sustainable Development and Artificial Intelligence.<br><br>Professional Experience: Dr. Isaak has played key roles in the public and private sectors, including his appointment to UN-Habitat’s Global Expert Group on Smart Cities, where he leads the people-centred Guides for the Development of Smart Cities. He is the founder and CEO of 'Neurabitat', a project that uses artificial intelligence and data analysis to improve interaction between cities and their inhabitants.<br><br>In the Foreign Relations Secretariat (SRE), he was the Urban General International Coordinator and author of the ICI project (Interconnecting Smart Cities), contributing to sustainable public policies. They highlight their negotiations to extend the mandate of Mexico as President of the UN AssemblyHabitat until 2025 and the allocation and upcoming celebration of World Habitat Day in Querétaro. <br><br>Vision and Future: Dr. Isaak is committed to creating smarter, more sustainable cities. His vision includes Smart City Innovation Centers and Digital Transformation Agencies in Mexico and LATAM, Integrating emerging technologies to benefit all citizens and promote sustainability. <br><br>In short, Dr. Hugo Isaak is an influential figure with an exemplary track record and a clear vision for a more sustainable and inclusive future.",
+            image: "/img/speakers/Hugo_Isaak.jpeg",
+            email: "",
+            phone: "",
+            moderador: true,
+          },
+          {
+            id: "2",
+            name: "Rodrigo Salcedo",
+            position: "Presidente",
+            position_en: "President",
+            company:
+              "Asociación Gremial de Vehículos Eléctricos de Chile (AVEC)",
+            bio: "Rodrigo Salcedo Campino es Ingeniero Comercial con una trayectoria en la industria logística y energética de Chile, especializándose en sistemas de transporte eléctrico y movilidad sostenible. <br><br>Es presidente de la Asociación Gremial de Vehículos Eléctricos de Chile (AVEC), donde impulsa políticas públicas y difusión del uso de vehículos eléctricos en el país. Es directivo en la Asociación Latinoamericana de Movilidad Sostenible (ALAMOS). <br><br>Ha sido expositor en foros y congresos internacionales - como Tomorrow.Mobility Barcelona y Smart City Expo en Barcelona - sobre electromovilidad y conectividad eléctrica en América Latina. <br><br>Con participación activa en comisiones legislativas nacionales y mesas técnicas, ha colaborado en iniciativas clave como la Ley de Electromovilidad y la estrategia regulatoria vigente en Chile.",
+            bio_en:
+              "Rodrigo Salcedo Campino is a Commercial Engineer with a background in the logistics and energy industry in Chile, specializing in electric transport systems and sustainable mobility. <br><br>He is president of the Association Gremial de Vehículos Eléctricos de Chile (AVEC), where he promotes public policies and dissemination of the use of electric vehicles in the country. Director of the Latin American Association for Sustainable Mobility (ALAMOS). <br><br>He has been an exhibitor in international forums and congresses - such as Tomorrow . Mobility Barcelona and Smart City Expo in Barcelona - on electromobility and electrical connectivity in Latin America. <br><br>With active participation in national legislative committees and technical tables, has collaborated on key initiatives such as the Electromobility Law and the current regulatory strategy in Chile.",
+            image: "/img/speakers/Rodrigo_Salcedo.png",
+            email: "",
+            phone: "",
+            moderador: false,
+          },
         ],
       },
     ],
