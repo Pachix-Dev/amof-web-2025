702773c7523479f7b1b502e7326e21c4721cc05b
JoseZR <josezerrod@hotmail.com>
2025-11-20 13:37:31 -0600

    se añaden precios de stands 2026

---
+++ src/constans_rates.js
@@ -0,0 +1,153 @@
+const rates_eco = {
+    "rates": [      
+        {
+            "deadline": {
+                "es": "Válido hasta 31/5/2025",
+                "en": "Available until 31/5/2025"
+            },
+            "title": {
+                "es": "Superficie Libre",
+                "en": "Open Surface"
+            },
+            "size": {
+                "es": "Medidas: 18 m² mínimo",
+                "en": "Size: 18 sqm min"
+            },
+            "early_price_title": {
+                "es": "Precio de Preventa",
+                "en": "Early Bird Price"
+            },
+            "regular_price_title": {
+                "es": "Precio Regular",
+                "en": "Regular Price"
+            },
+            "early_price": {
+                "es": "6,400 MXN / m² + IVA",
+                "en": "6,400 MXN / sqm + VAT"
+            },
+            "regular_price": {
+                "es": "6,720 MXN / m² + IVA",
+                "en": "6,720 MXN / sqm + VAT"
+            },
+            "includes_title": {
+                "es": "Incluye",
+                "en": "Includes"
+            },
+            "not_included": {
+                "es": "NO incluye alfombra, construcción, mobiliario, ni servicios.",
+                "en": "Does NOT include carpet, construction, furniture, or services."
+            },
+            "items": [
+                {
+                    "description": {
+                        "es": "Incluye gafetes de expositor (3 por cada 9 m²)",
+                        "en": "Exhibitor badges (3 per 9 sqm)"
+                    }
+                },
+                {
+                    "description": {
+                        "es": "Código de preregistro de visitantes ilimitados para su compañía",
+                        "en": "Unlimited visitor pre-registration code for your company"
+                    }
+                },
+                {
+                    "description": {
+                        "es": "Mención en el directorio",
+                        "en": "Mention in the directory"
+                    }
+                }
+            ]
+        },
+        {
+            "deadline": {
+                "es": "Válido hasta 31/5/2025",
+                "en": "Available until 31/5/2025"
+            },
+            "title": {
+                "es": "Stand Equipado",
+                "en": "Equipped Booth"
+            },
+            "size": {
+                "es": "Medidas: 9 m² mínimo",
+                "en": "Size: 9 sqm min"
+            },
+            "early_price_title": {
+                "es": "Precio de preventa",
+                "en": "Early Bird"
+            },
+            "regular_price_title": {
+                "es": "Precio Regular",
+                "en": "Regular Price"
+            },
+            "early_price": {
+                "es": "7,400 MXN / m² + IVA",
+                "en": "7,400 MXN / sqm + VAT"
+            },
+            "regular_price": {
+                "es": "7,800 MXN / m² + IVA",
+                "en": "7,800 MXN / sqm + VAT"
+            },
+            "includes_title": {
+                "es": "Incluye",
+                "en": "Includes"
+            },
+            "not_included": {
+                "es": "",
+                "en": ""
+            },
+            "items": [
+                {
+                    "description": {
+                        "es": "Alfombra estándar",
+                        "en": "Standard carpet"
+                    }
+                },
+                {
+                    "description": {
+                        "es": "Mamparas divisorias",
+                        "en": "Dividing partitions"
+                    }
+                },
+                {
+                    "description": {
+                        "es": "Antepecho con rótulo en vinil",
+                        "en": "Vinyl sign"
+                    }
+                },
+                {
+                    "description": {
+                        "es": "Mobiliario estándar (counter, mesa y 2 sillas)",
+                        "en": "Standard furniture (counter, table and 2 chairs)"
+                    }
+                },
+                {
+                    "description": {
+                        "es": "1 contacto eléctrico de 110v",
+                        "en": "One 110v electrical contact"
+                    }
+                },
+                {
+                    "description": {
+                        "es": "Gafetes de expositor (3 por cada 9 m²)",
+                        "en": "Exhibitor badges (3 per 9 sqm)"
+                    }
+                },
+                {
+                    "description": {
+                        "es": "Código de preregistro de visitantes ilimitados para su compañía",
+                        "en": "Unlimited visitor pre-registration code for your company"
+                    }
+                },
+                {
+                    "description": {
+                        "es": "Mención en el directorio",
+                        "en": "Mention in the directory"
+                    }
+                }
+            ]
+        },
+        
+    ]
+}
+
+export { rates_eco }
